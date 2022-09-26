57447c37d5425556d8fbca68382fbdd9804eaed0
Maks2712 <maks2712@yahoo.com>
2022-09-26 23:09:25 +0700

    Basic setup completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   get 'users/index'
   devise_for :users
-
+  root "users#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resource :user do
 
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
